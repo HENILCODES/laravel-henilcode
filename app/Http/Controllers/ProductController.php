eb8695cb4c085e38cab1ed517974869c228bf089
henilcode <henilcode@gmail.com>
2023-01-23 22:17:17 +0530

    complete create delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,39 +7,18 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
         $product = new Product();
         $products = $product::all();
-        return view('product.index')->with('products',$products);
+        return view('product.index')->with('products', $products);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
         return view('product.index')->with('create', true);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
         $product = new Product();
         $product->name = $request['name'];
         $product->price = $request['price'];
@@ -47,39 +26,17 @@ class ProductController extends Controller
         $product->save();
         return redirect('product/');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $product = Product::find($id);
         echo "<pre>";
         print_r($product->id);
-    }   
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    }
     public function edit($id)
     {
         $product = Product::find($id);
         return $product;
-        // return view('product.index')->with('editProduct',$product);
     }
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $product = Product::find($id);
@@ -92,16 +49,8 @@ class ProductController extends Controller
         // return view('product.index');
         return "Edit";
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
         $product = Product::find($id);
         $product->delete();
         return redirect()->back();
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,25 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-</head>
+<title>Product</title>
 
 <body>
-
+    @extends('layout')
     {{-- Edit Product  --}}
-    @if (isset($editProduct))
+    @section('body')
         <div class="modal" id="EditProduct">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Product</h1>
-                        <a type="button" class="btn-close" href="{{ url('product/') }}"></a>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                     </div>
                     <div class="modal-body">
                         <form class="row g-3 m-auto" autocomplete="off" action="{{ url('product/') }}" method="put"
@@ -28,13 +18,13 @@
                             @csrf
                             <div class="input-group">
                                 <spna class="fw-bold input-group-text justify-content-center"> Name</spna>
-                                <input required type="text" class="form-control" name="name"
-                                    placeholder="enter product name" value="{{ $editProduct->name }}">
+                                <input required type="text" class="form-control" name="name" id="name"
+                                    placeholder="enter product name">
                             </div>
                             <div class="input-group">
                                 <spna class="fw-bold input-group-text justify-content-center"> price</spna>
-                                <input required type="number" class="form-control" name="price"
-                                    placeholder="enter product price" value="{{ $editProduct->price }}">
+                                <input required type="number" class="form-control" name="price" id="price"
+                                    placeholder="enter product price">
                             </div>
                             <div class="input-group">
                                 <spna class="fw-bold input-group-text justify-content-center">image</spna>
@@ -44,142 +34,140 @@
                         </form>
                     </div>
                     <div class="modal-footer">
-                        <a type="button" class="btn btn-secondary" href="{{ url('product/') }}">Close</a>
-                        {{-- Click Close to redirect index --}}
+                        <a type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</a>
                     </div>
                 </div>
             </div>
         </div>
-    @endif
 
-    {{-- Add New Product --}}
-    <div class="modal" id="AddProduct">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">New Product</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container my-3">
-                        <form class="row g-3 m-auto" autocomplete="off" action="{{ url('product/') }}" method="post"
-                            enctype="multipart/form-data">
-                            @method('POST')
-                            @csrf
-                            <div class="input-group">
-                                <spna class="fw-bold input-group-text justify-content-center"> Name</spna>
-                                <input required type="text" class="form-control" name="name"
-                                    placeholder="enter product name">
-                            </div>
-                            <div class="input-group">
-                                <spna class="fw-bold input-group-text justify-content-center"> price</spna>
-                                <input required type="number" class="form-control" name="price"
-                                    placeholder="enter product price">
-                            </div>
-                            <div class="input-group">
-                                <spna class="fw-bold input-group-text justify-content-center">image</spna>
-                                <input type="file" name="photo" class="form-control" accept="image/*">
-                            </div>
-                            <div class="mt-5 text-center">
-                                <button class="btn btn-primary">Add Product</button>
-                            </div>
-                        </form>
+        {{-- Add New Product --}}
+        <div class="modal" id="AddProduct">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">New Product</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    <div class="modal-body">
+                        <div class="container my-3">
+                            <form class="row g-3 m-auto" autocomplete="off" action="{{ url('product/') }}" method="post"
+                                enctype="multipart/form-data">
+                                @method('POST')
+                                @csrf
+                                <div class="input-group">
+                                    <spna class="fw-bold input-group-text justify-content-center"> Name</spna>
+                                    <input required type="text" class="form-control" name="name"
+                                        placeholder="enter product name">
+                                </div>
+                                <div class="input-group">
+                                    <spna class="fw-bold input-group-text justify-content-center"> price</spna>
+                                    <input required type="number" class="form-control" name="price"
+                                        placeholder="enter product price">
+                                </div>
+                                <div class="input-group">
+                                    <spna class="fw-bold input-group-text justify-content-center">image</spna>
+                                    <input type="file" name="photo" class="form-control" accept="image/*">
+                                </div>
+                                <div class="mt-5 text-center">
+                                    <button class="btn btn-primary">Add Product</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <section style="background-color: #eee;" class="shadow rounded ps-5 pe-5">
-            <div class="container my-5 py-4">
-                <div class="text-center">
-                    <h2 class="fw-bold">Product</h2>
-                </div>
-                <div class="text-end pb-4 d-flex justify-content-between">
-                    <input type="search" autocomplete="off" class="form-control w-25 me-5" id="search"
-                        placeholder="search">
-                    <button class="btn btn-success shadow" data-bs-toggle="modal"
-                        data-bs-target="#AddProduct">Add</button>
-                </div>
-                <div class="overflow-auto" style="height: 600px;">
+        <div class="container">
+            <section style="background-color: #eee;" class="shadow rounded ps-5 pe-5">
+                <div class="container my-5 py-4">
+                    <div class="text-center">
+                        <h2 class="fw-bold">Product</h2>
+                    </div>
+                    <div class="text-end pb-4 d-flex justify-content-between">
+                        <input type="search" autocomplete="off" class="form-control w-25 me-5" id="search"
+                            placeholder="search">
+                        <button class="btn btn-success shadow" data-bs-toggle="modal"
+                            data-bs-target="#AddProduct">Add</button>
+                    </div>
+                    <div class="overflow-auto" style="height: 600px;">
 
-                    <table class="table table-primary text-center table-responsive table-bordered">
-                        <thead class="table-borderless table-dark">
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>price</th>
-                            <th>photo</th>
-                            <th>Update time</th>
-                            <th>Create time</th>
-                            <th>Action</th>
-                        </thead>
-                        <tbody id="Search_table" class="overflow-auto">
-                            @if (isset($products))
-                                @foreach ($products as $key => $value)
-                                    <tr>
-                                        <td> {{ $value->id }}</td>
-                                        <td> {{ $value->name }}</td>
-                                        <td> {{ $value->price }}</td>
-                                        <td> {{ $value->photo }}</td>
-                                        <td> {{ $value->updated_at }}</td>
-                                        <td> {{ $value->created_at }}</td>
-                                        <td class="d-flex">
-                                            {{-- Edit Form data button --}}
-                                            <a class="btn btn-success shadow bi bi-pencil me-3 editBT"></a>
-                                            {{-- <a class="btn btn-success shadow bi bi-pencil me-3 "
+                        <table class="table table-primary text-center table-responsive table-bordered">
+                            <thead class="table-borderless table-dark">
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>price</th>
+                                <th>photo</th>
+                                <th>Update time</th>
+                                <th>Create time</th>
+                                <th>Action</th>
+                            </thead>
+                            <tbody id="Search_table" class="overflow-auto">
+                                @if (isset($products))
+                                    @foreach ($products as $key => $value)
+                                        <tr>
+                                            <td> {{ $value->id }}</td>
+                                            <td> {{ $value->name }}</td>
+                                            <td> {{ $value->price }}</td>
+                                            <td> {{ $value->photo }}</td>
+                                            <td> {{ $value->updated_at }}</td>
+                                            <td> {{ $value->created_at }}</td>
+                                            <td class="d-flex">
+                                                {{-- Edit Form data button --}}
+                                                <button id="{{ $value->id }}"
+                                                    class="btn btn-success shadow bi bi-pencil me-3 editBT"></button>
+                                                {{-- <a class="btn btn-success shadow bi bi-pencil me-3 "
                                                 href="/product/{{ $value->id }}/edit"></a> --}}
 
-                                            {{-- <form action="/photo/" method="DELETE"> --}}
-                                            {{-- Delete Form data button --}}
-                                            {{-- @method('DELETE') --}}
-                                            {{-- @csrf --}}
-                                            {{-- <input type="hidden" name="id" value="{{ $value->id }}"> --}}
-                                            {{-- <button type="submit" class="btn btn-danger bi bi-trash"></button> --}}
-                                            {{-- </form> --}}
-                                            <a href="{{ route('product-delete', ['id' => $value->id]) }}"
-                                                class="btn btn-danger bi bi-trash"></a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
+                                                {{-- <form action="/photo/" method="DELETE"> --}}
+                                                {{-- Delete Form data button --}}
+                                                {{-- @method('DELETE') --}}
+                                                {{-- @csrf --}}
+                                                {{-- <input type="hidden" name="id" value="{{ $value->id }}"> --}}
+                                                {{-- <button type="submit" class="btn btn-danger bi bi-trash"></button> --}}
+                                                {{-- </form> --}}
+                                                <a href="{{ route('product-delete', ['id' => $value->id]) }}"
+                                                    class="btn btn-danger bi bi-trash"></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </section>
-    </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-<script src="../js/search.js"></script>
-@if (isset($create))
-    <script type="text/javascript">
-        $(window).on('load', function() {
-            $('#AddProduct').modal('show');
+            </section>
+        </div>
+    </body>
+
+    @if (isset($create))
+        <script type="text/javascript">
+            $(window).on('load', function() {
+                $('#AddProduct').modal('show');
+            });
+        </script>
+    @endif
+    <script>
+        $(document).ready(function() {
+            $(".editBT").on("click", function() {
+                let id = $(this).attr('id');
+                $.ajax('http://127.0.0.1:8000/product/' + id + '/edit', {
+                    type: 'GET',
+                    dataType: 'json',
+                    error: function(jqObj, textstatus, errorMessage) {
+                        alert(errorMessage);
+                    },
+                    success: function(data) {
+                        $('#name').val(data['name'])
+                        $('#price').val(data['price'])
+                        $('#EditProduct').modal('show');
+                    }
+                });
+            });
         });
     </script>
-@endif
-@if (isset($editProduct))
-    <script type="text/javascript">
-        $(window).on('load', function() {
-            $('#EditProduct').modal('show');
-        });
-    </script>
-@endif
-<script>
-    $(document).ready(function() {
-        $(".editBT").on("click", function() {
-           $.ajax('http://127.0.0.1:8000/product/3/edit',{
-            success:function(data){
 
-                console.log(data);
-            }
-           })
-        });
-    });
-</script>
-
-</html>
+    </html>
+@endsection
