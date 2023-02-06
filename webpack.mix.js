b27db034668be05e378ff480f775ffc17b81e22c
henilcode <henilcode@gmail.com>
2023-02-06 18:32:26 +0530

    mix set

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/bootstraps.js', 'js');
+mix.js('resources/js/bootstrap.js', 'dist');
 mix.css('resources/css/bootstrap.min.css','css');
+mix.css('resources/css/test.css','css');
